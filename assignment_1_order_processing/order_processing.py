f2c0166e4d89dd5448ff1b7e0ccffb8721281022
Abhishek <abhishek0saini@gmail.com>
2021-10-15 18:22:59 -0700

    initiate creation of deque and dict

--- assignment_1_order_processing/order_processing.py
+++ assignment_1_order_processing/order_processing.py
@@ -1,18 +1,50 @@
-class Order():
-    def __init__(self, order_id, type_of_order, properties_dict):
+from collections import deque
+import copy
+
+
+class OrderType(object):
+    BOOK = 'Book'
+    MATERIAL = 'Material'
+    FOOD = 'Food'
+
+
+class Order(object):
+    def __init__(self, order_id, order_type, properties_dict):
         self.order_id = order_id
-        self.type = type_of_order
+        self.order_type = order_type
         self.properties_dict = properties_dict
 
-    def edit(self, type_of_order, properties_dict):
-        self.type = type_of_order
-        self.properties_dict = properties_dict
 
-class OrderProcessor():
+class OrderProcessor(object):
     def __init__(self, orders_to_be_processed):
-        self.orders_to_be_processed = orders_to_be_processed
+        """
+        creates an OrderProcessor class from a list of order objects
+        """
+        self.to_be_processed_deque = self._convert_orders_to_deque(
+            orders_to_be_processed)
+        self.to_be_processed_dict = self._convert_orders_to_dict(
+            orders_to_be_processed)
+
+    def _convert_orders_to_deque(self, orders):
+        """
+        this method creates a deque from the order ids
+        this deque of order ids will be used to maintain FIFO
+        processing of the orders
+        """
+        order_id_deque = deque()
+        for order in orders:
+            order_id_deque.append(order.order_id)
+        return order_id_deque
+
+    def _convert_orders_to_dict(self, orders):
+        """
+        this method creates a dictionary from the orders
+        this dictionary is used for O(1) access for reporting
+        """
+        order_id_dict = {}
+        for order in orders:
+            order_id_dict[order.order_id] = copy.deepcopy(order)
+        return order_id_dict
 
     def add_order_to_be_processed(self, order):
         self.orders_to_be_processed.append(order)
-
-    
---
+++ assignment_1_order_processing/tests/test_order_processing.py
@@ -0,0 +1,65 @@
+import pytest
+from ..order_processing import OrderType, Order, OrderProcessor
+from collections import deque
+
+
+def test_init_Order():
+    # Arrange
+    order_id = 1
+    order_type = OrderType.BOOK
+    properties_dict = {
+        "title": "Snow Crash",
+        "author": "Neal Stephenson",
+        "price": 14.99,
+        "no_items": 1
+    }
+
+    # Act
+    order = Order(order_id, order_type, properties_dict)
+
+    # Assert
+    assert order.order_type == order_type
+
+
+def test_init_OrderProcessing_deque():
+    # Arrange
+    order_id = 1
+    order_type = OrderType.BOOK
+    properties_dict = {
+        "title": "Snow Crash",
+        "author": "Neal Stephenson",
+        "price": 14.99,
+        "no_items": 1
+    }
+    order = Order(order_id, order_type, properties_dict)
+
+    orders_to_be_processed = [order]
+
+    # Act
+    order_processor = OrderProcessor(orders_to_be_processed)
+
+    # Assert
+    assert order_processor.to_be_processed_deque \
+        == deque([1])
+
+
+def test_init_OrderProcessing_dict():
+    # Arrange
+    order_id = 1
+    order_type = OrderType.BOOK
+    properties_dict = {
+        "title": "Snow Crash",
+        "author": "Neal Stephenson",
+        "price": 14.99,
+        "no_items": 1
+    }
+    order = Order(order_id, order_type, properties_dict)
+
+    orders_to_be_processed = [order]
+
+    # Act
+    order_processor = OrderProcessor(orders_to_be_processed)
+
+    # Assert
+    assert order_processor.to_be_processed_dict[1].properties_dict \
+        == properties_dict
